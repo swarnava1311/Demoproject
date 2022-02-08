9d5140e07f51d9f150fbf1164a44290ad160e0a2
swarnava1311 <72728415+swarnava1311@users.noreply.github.com>
2022-02-08 14:25:52 +0530

    Update demo_test.sh

--- demo_test.sh
+++ demo_test.sh
@@ -1,5 +1,5 @@
 yum update all
 yum install httpd -y
 systemctl start httpd
-sudo echo " Welcome to the host `hostname` and today is `date`. You can reach to me using curl http://169.254.169.254/latest/meta-data/public-ipv4` IP details" > /var/www/html/index.html
+sudo echo " Welcome this is Jenkins Pipeline Job" > /var/www/html/index.html
 
