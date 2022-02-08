97b71ff4abbb8c3deb30583ce24a48a3946ff473
swarnava1311 <72728415+swarnava1311@users.noreply.github.com>
2022-02-08 14:02:29 +0530

    Update demo_test.sh

--- demo_test.sh
+++ demo_test.sh
@@ -1,3 +1,5 @@
-yum update all
-yum install httpd -y
-systemctl start httpd
+sudo yum update all
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo echo " Welcome to the host `hostname` and today is `date`. You can reach to me using curl http://169.254.169.254/latest/meta-data/public-ipv4` IP details" > /var/www/html/index.html
+
