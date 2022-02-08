96dc09f4b0b5bf81046a97a3329312ab55c5d146
swarnava1311 <72728415+swarnava1311@users.noreply.github.com>
2022-02-08 13:46:31 +0530

    Update demo_test.sh

--- demo_test.sh
+++ demo_test.sh
@@ -1,4 +1,3 @@
 yum update all
 yum install httpd -y
-echo " Welcome to the host `hostname` and today is `date`. You can reach to me using curl http://169.254.169.254/latest/meta-data/public-ipv4` IP details" > /var/www/html/index.html
 systemctl start httpd
