7caa87d70c9f072905f5c3a1ae7400b9f7056a40
swarnava1311 <72728415+swarnava1311@users.noreply.github.com>
2022-02-08 14:05:11 +0530

    Update demo_test.sh

--- demo_test.sh
+++ demo_test.sh
@@ -1,5 +1,5 @@
 sudo yum update all
 sudo yum install httpd -y
 sudo systemctl start httpd
-sudo echo " Welcome to the host `hostname` and today is `date`. You can reach to me using curl http://169.254.169.254/latest/meta-data/public-ipv4` IP details" > /var/www/html/index.html
+sudo echo "Hello World!! This is Jenkins test" > /var/www/html/index.html
 
